d69d03da4838a5f1201242119e61f85fe69eed9f
Tiago Floriano <tjfreelancer@mail.com>
2015-06-24 16:10:35 -0300

    Estrutura inicial

---
+++ index.php
@@ -0,0 +1,18 @@
+<?
+session_start();
+/*
+    LICENÇA GPL 2
+
+    Este programa é um software livre; você pode redistribuí-lo e/ou 
+    modificá-lo dentro dos termos da Licença Pública Geral GNU como 
+    publicada pela Fundação do Software Livre (FSF); na versão 2.
+
+    Este programa é distribuído na esperança de que possa ser útil, 
+    mas SEM NENHUMA GARANTIA; sem uma garantia implícita de ADEQUAÇÃO
+    a qualquer MERCADO ou APLICAÇÃO EM PARTICULAR. Veja a
+    Licença Pública Geral GNU para maiores detalhes.
+
+    Você deve ter recebido uma cópia da Licença Pública Geral GNU junto
+    com este programa. Se não, veja <http://www.gnu.org/licenses/>.
+ */
+
